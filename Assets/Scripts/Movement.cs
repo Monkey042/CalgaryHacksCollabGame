6762867d81d89aaef0002573c129197982f98627
Monkey042 <Monkey042042@gmail.com>
2025-02-15 13:03:44 -0700

    Movement started

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,16 +1,21 @@
-using UnityEngine;
+using UnityEngine; 
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float speed;
+    private Rigidbody2D rb;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float horizontalMove = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed * 10;
+
+        print(horizontalMove);
+
+        rb.linearVelocity = new Vector2(horizontalMove, rb.linearVelocity.y);
     }
 }
