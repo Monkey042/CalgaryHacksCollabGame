0fe0bb1ca5ec70beb5639ef7f4bec9efa2ba9944
Monkey042 <Monkey042042@gmail.com>
2025-02-15 14:18:51 -0700

    Player Movement Update, Prefab

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,21 +1,41 @@
 using UnityEngine; 
 
+public enum MoverType { Basic, Jumper, Slower, Shrinker, Pusher }
+
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D rb;
+
+    public MoverType moverType;
+
+    public KeyCode jumpKey = KeyCode.Space;
+    public float jumpForce;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        if (moverType == MoverType.Jumper)
+        {
+            Jumper();
+        }
+    }
+
     private void FixedUpdate()
     {
-        float horizontalMove = Input.GetAxisRaw("Horizontal") * speed * 10;
-
-        print(horizontalMove);
-
+        float horizontalMove = Input.GetAxis("Horizontal") * speed * 10;
         rb.linearVelocity = new Vector2(horizontalMove, rb.linearVelocity.y);
+    }
+
+    private void Jumper()
+    {
+        if (Input.GetKeyDown(jumpKey))
+        {
+            rb.AddRelativeForceY(jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
